14f87ee169997dadea46adee6ef99d3788783ead
luis2003 <luis2003@gmail.com>
2023-01-28 17:58:15 -0300

    Add starter code, basic unit test, updated .gitignore

---
+++ tests/test_order.py
@@ -0,0 +1,12 @@
+import unittest
+from single_responsibility import Order
+
+
+class OrderTestCase(unittest.TestCase):
+    def test_order_creation_ok(self):
+        an_order = Order()
+        self.assertIsInstance(an_order, Order)
+
+
+if __name__ == '__main__':
+    unittest.main()
