6fa5416fef684728cd3eabd19b40e09f262c0f3c
luis2003 <luis2003@gmail.com>
2023-01-29 20:50:04 -0300

    refactor/test: Modify PaymentProcessor to use OCP/Adapt related tests

--- payment_system/payment_processor.py
+++ payment_system/payment_processor.py
@@ -1,13 +1,24 @@
-class PaymentProcessor:
+from abc import ABC, abstractmethod
+
+
+class PaymentProcessor(ABC):
     def __init__(self, security_code):
         self.security_code: str = security_code
 
-    def pay_debit(self, an_order):
+    @abstractmethod
+    def pay(self, an_order):
+        pass
+
+
+class DebitPaymentProcessor(PaymentProcessor):
+    def pay(self, an_order):
         print("Processing debit payment type")
         print(f"Verifying security code: {self.security_code}")
         an_order.status = "paid"
 
-    def pay_credit(self, an_order):
+
+class CreditPaymentProcessor(PaymentProcessor):
+    def pay(self, an_order):
         print("Processing credit payment type")
         print(f"Verifying security code: {self.security_code}")
         an_order.status = "paid"
--- tests/test_payment_processor.py
+++ tests/test_payment_processor.py
@@ -1,23 +1,21 @@
 import unittest
-from payment_system.payment_processor import PaymentProcessor
+from payment_system.payment_processor import DebitPaymentProcessor, CreditPaymentProcessor
 from payment_system.order import Order
 
 
 class PaymentProcessorTestCase(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.a_processor = PaymentProcessor("0372846")
+        self.a_debit_processor = DebitPaymentProcessor("0372846")
+        self.a_credit_processor = CreditPaymentProcessor("0372846")
         self.an_order = Order()
 
-    def test_creation(self):
-        self.assertIsInstance(self.a_processor, PaymentProcessor)
-
     def test_pay_debit(self):
-        self.a_processor.pay_debit(self.an_order)
+        self.a_debit_processor.pay(self.an_order)
         self.assertEqual("paid", self.an_order.status)
 
     def test_pay_credit(self):
-        self.a_processor.pay_credit(self.an_order)
+        self.a_credit_processor.pay(self.an_order)
         self.assertEqual("paid", self.an_order.status)
 
 
