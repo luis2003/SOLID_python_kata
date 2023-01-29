721972d5454145affb2d282498f3dad7b21b64f2
luis2003 <luis2003@gmail.com>
2023-01-29 17:47:49 -0300

    test: Enhance test suite with new unit test, setUp and skip redundant unit test

--- tests/test_order.py
+++ tests/test_order.py
@@ -3,9 +3,18 @@ from payment_system.order import Order
 
 
 class OrderTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.an_order = Order()
+
+    @unittest.skip("This test is redundant with SetUp")
     def test_order_creation_ok(self):
-        an_order = Order()
-        self.assertIsInstance(an_order, Order)
+        self.assertIsInstance(self.an_order, Order)
+
+    def test_total_price_multiple_items_with_repeated_item(self):
+        self.an_order.add_item("Keyboard", 1, 50)
+        self.an_order.add_item("SSD", 1, 150)
+        self.an_order.add_item("USB cable", 2, 5)
+        self.assertEqual(210, self.an_order.total_price())
 
 
 if __name__ == '__main__':
