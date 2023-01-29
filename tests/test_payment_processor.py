6e140973ecc2b26561e6cd97e7a92dfa124f2551
luis2003 <luis2003@gmail.com>
2023-01-29 17:36:44 -0300

    test: refactor paymentprocessor test case using setUp fixture

--- tests/test_payment_processor.py
+++ tests/test_payment_processor.py
@@ -4,21 +4,21 @@ from payment_system.order import Order
 
 
 class PaymentProcessorTestCase(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.a_processor = PaymentProcessor("0372846")
+        self.an_order = Order()
+
     def test_creation(self):
-        a_processor = PaymentProcessor("0372846")
-        self.assertIsInstance(a_processor, PaymentProcessor)
+        self.assertIsInstance(self.a_processor, PaymentProcessor)
 
     def test_pay_debit(self):
-        a_processor = PaymentProcessor("0372846")
-        an_order = Order()
-        a_processor.pay_debit(an_order)
-        self.assertEqual("paid", an_order.status)
+        self.a_processor.pay_debit(self.an_order)
+        self.assertEqual("paid", self.an_order.status)
 
     def test_pay_credit(self):
-        a_processor = PaymentProcessor("0372846")
-        an_order = Order()
-        a_processor.pay_credit(an_order)
-        self.assertEqual("paid", an_order.status)
+        self.a_processor.pay_credit(self.an_order)
+        self.assertEqual("paid", self.an_order.status)
 
 
 if __name__ == '__main__':
