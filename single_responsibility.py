809d09143d60cc9487f5a2a45927e38019f7771a
luis2003 <luis2003@gmail.com>
2023-01-29 10:26:18 -0300

    [Feat.] Add final pay_debit method

--- single_responsibility.py
+++ single_responsibility.py
@@ -42,3 +42,9 @@ order.pay("debit", "0372846")
 class PaymentProcessor:
     def __init__(self, security_code):
         self.security_code: str = security_code
+
+    def pay_debit(self, an_order):
+        print("Processing debit payment type")
+        print(f"Verifying security code: {self.security_code}")
+        an_order.status = "paid"
+
